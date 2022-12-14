13609c3b420bfd16328713fc0fcd1946b39a0eae
Liza McLain <lizamclain@gmail.com>
2022-12-13 22:48:45 -0600

    commit tree 6a2cefebf10dac50474c5367d2d04d12dbd32f2b

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.textContent = "LIZA is the champion";
+
+document.body.append(h1);
